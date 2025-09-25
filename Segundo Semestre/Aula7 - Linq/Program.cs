f34c3ed0a3600504d50d46ff13d6885fe369e03f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-25 11:46:26 -0300

    Aula 7 em sala

---
+++ Segundo Semestre/Aula7 - Linq/Program.cs
@@ -0,0 +1,38 @@
+﻿using LinqRM.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+using (var client = new HttpClient())
+{
+    var characters = new List<Character>();
+
+    var currentPage = await client.GetFromJsonAsync<CharacterPage>("https://rickandmortyapi.com/api/character");
+
+    characters.AddRange(currentPage.results);
+
+    while(currentPage.info.next != null)
+    {
+        currentPage = await client.GetFromJsonAsync<CharacterPage>(currentPage.info.next);
+        characters.AddRange(currentPage.results);
+    }
+
+    //var filteredChars = characters.Where( c => c.status == "Alive")
+    //                              .Where(c => c.species == "Human")
+    //                              .OrderByDescending(c => c.episode.Length)
+    //                              .ThenBy(c => c.name)
+    //                              .ToList();
+
+
+    var filteredChars = characters.Where(c => c.status == "Alive")
+                                  //.Where(c => c.species == "Human")
+                                  //.Where(c => c.episode.Length > 2)
+                                  .GroupBy(c => c.species);
+
+    foreach (var group in filteredChars)
+    {
+        Console.WriteLine(group.Key);
+    }
+
+    Console.WriteLine(filteredChars.Count());
+
+}
