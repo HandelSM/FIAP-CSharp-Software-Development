b945b35d325749b03f5c16f5ba831e3bb3525120
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-02 11:30:27 -0300

    exemplos em aula de Parallel

--- Segundo Semestre/Aula8 - Async/Async/Program.cs
+++ Segundo Semestre/Aula8 - Async/Async/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = 10_000_000;
+
+int sum = 0;
+
+//for (int i = 0; i<N; i++)
+//{
+//    sum++;
+//}
+
+//Console.WriteLine(sum);
+
+//Parallel.For(0, N, i =>
+//{
+//    sum += 1;
+//});
+
+//var gate = new Object();
+
+//Parallel.For(0, N, i =>
+//{
+//    lock (gate)
+//    {
+//        sum += 1;
+//    }
+//});
+
+Parallel.For(0, N, i =>
+{
+    //Interlocked.Add(ref sum, 1);
+    Interlocked.Increment(ref sum);
+});
+
+Console.WriteLine(sum);
+
